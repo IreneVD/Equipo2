b42f217d176d98204ae7a79e08be7c3bb5dd3c6e
irene <irene@Billy>
2023-04-26 12:08:47 +0200

    Modificado por la jefa del Equipo2.

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author irene
+ *
+ */
+module Equipo2 {
+}
---
+++ src/pkEquipo2/Hola.java
@@ -0,0 +1,10 @@
+package pkEquipo2;
+
+public class Hola {
+
+	public static void main(String[] args) {
+		System.out.println("Hola soy la jefa del Equipo2");
+
+	}
+
+}
