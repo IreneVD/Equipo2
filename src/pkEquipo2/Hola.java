34203ab672dc2688499d310aabc10403727798dd
mtirador <125604629+mtirador@users.noreply.github.com>
2023-04-26 12:17:46 +0200

    modificado por Marta

--- src/pkEquipo2/Hola.java
+++ src/pkEquipo2/Hola.java
@@ -5,7 +5,7 @@ public class Hola {
 	public static void main(String[] args) {
 		System.out.println("Hola soy la jefa del Equipo2");
 		System.out.println("Hazte una gayola mi cuco");
-
+		System.out.println("holaa");
 	}
 
 }
